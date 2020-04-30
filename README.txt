bb788a9b56017c1a371114ca6d1281ab53067514
NotVeryGoodAtCodingButOk <simonboteroaristi@gmail.com>
2020-04-30 08:13:18 -0500

    Deleted thye .idea files

---
+++ README.txt
@@ -0,0 +1,5 @@
+Hello, fellow human being.
+THis is a free game, coded from 4/27/20 to
+This game has no copyright, meaning that you can use it or modify it as you may!
+You will need to have PYGAME and python installed: especifically, python 3.7.7 (visit python.org for a download)
+
--- thehelicoptergame.py
+++ thehelicoptergame.py
@@ -4,24 +4,35 @@
 # A project using Pygame 2.0.0.dev6, with the Python 3.7.7 interpreter, and the PyCharm IDE
 
 # Import the modules we will be using for this project.
-import pygame 
+import pygame
 from time import sleep
 from sys import exit
-import os 
+from os import path
 
 # Initialize (or enable) all of the pygame functionality!
 pygame.mixer.init()
 pygame.font.init()
 
 # Define some of the variables we will be using
-HEIGHT, WIDTH = (800, 800) # Creates a variable that stores our screen width and height
+HEIGHT, WIDTH = (800, 800)  # Creates a variable that stores our screen width and height
 score = 0
-FONT = pygame.font.SysFont('arial', 75, True, False, None)
+FONT = pygame.font.SysFont('arial', 50)
 score_label = FONT.render(f"Score: {score}", 1, (250, 250, 250))
+
+# While setting up our variables, let's load our images
+FILE_FORMAT = "png"
+COIN = "helicopterCoin" + FILE_FORMAT
+CLOUD =
 
 # Creating the screen and setting it up (or Suface object, in technical terms)
 WIN = pygame.display.set_mode((HEIGHT, WIDTH))  # Creates the screen object and stores
 pygame.display.set_caption("The helicopter Game by SB")  # Sets the screen caption
+
+
+# I will now create functions to display and do various things
+def score_label_on_screen():
+    WIN.blit(score_label, (WIDTH - score_label.get_width() - 10, 0 + score_label.get_width()/8 - 20))
+
 
 # Creating a clock object, which lets us schedule the screen refresh
 clock = pygame.time.Clock()
@@ -33,4 +44,5 @@ while True:
             # pygame.quit())
             pygame.quit()  # Quit the screen
             exit()  # Exits any current python project, from the sys module we imported earlier
+    score_label_on_screen()
     pygame.display.update()  # Update the display
